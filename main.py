b559225d5f3fc8b6755d8aa39b4fc75a16ce73ab
Tao Hansen <59834693+worldofgeese@users.noreply.github.com>
2023-06-06 15:11:45 +0200

    Create functional application

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
-from motor.motor_asyncio import AsyncIOMotorClient
-from bson import ObjectId
-from decouple import config
+import sys
+import traceback
 
-app = FastAPI()
+import uvicorn
 
-client = AsyncIOMotorClient(config("MONGO_URL"))
-db = client[config("MONGO_DB")]
+print("Starting...")
 
-class User(BaseModel):
-    id: str = None
-    social_media_username: str
-    active: bool = True
+try:
+    import time
+    from typing import List
 
-@app.get("/", response_model=List[User])
-async def read_root():
-    users = []
-    async for user in db.users.find():
-        user["id"] = str(user["_id"])
-        del user["_id"]
-        users.append(user)
-    return users
+    from decouple import config
+    from fastapi import FastAPI
+    from motor.motor_asyncio import AsyncIOMotorClient
+    from pydantic import BaseModel
+
+    print("Modules imported successfully.")
+
+    app = FastAPI()
+
+    client = None
+    while not client:
+        try:
+            client = AsyncIOMotorClient(config("MONGO_URL"))
+        except Exception as e:
+            print(f"Unable to connect to MongoDB. Error: {e}")
+            print("Retrying in 5 seconds...")
+            time.sleep(5)
+
+    print("Connected to MongoDB.")
+
+    db = client[config("MONGO_DB")]
+
+    class User(BaseModel):
+        id: str = None
+        social_media_username: str
+        active: bool = True
+
+    @app.get("/", response_model=List[User])
+    async def read_root():
+        users = []
+        if db:
+            async for user in db.users.find():
+                user["id"] = str(user["_id"])
+                del user["_id"]
+                users.append(user)
+        return users
+
+    print("FastAPI application setup complete.")
+
+except Exception as e:
+    print("An exception occurred:", e)
+    traceback.print_exc(file=sys.stdout)
+
+print("End of script.")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
